a9d4710090be443cce264ca5955d373bc67e95f8
Yassine <yassine.mjardi@gmail.com>
2025-12-01 16:32:47 +0100

    Initial commit: next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // For GitHub Pages compatibility
+  images: { unoptimized: true }
+}
+module.exports = nextConfig
+    
